59114a9b192cdcd98c3873aed72cbd3d540f3589
molla <hsjang635@naver.com>
2024-12-09 17:59:08 +0900

    implement file upload

---
+++ js/button.js
@@ -0,0 +1,35 @@
+const LogUploadBtn = document.getElementById("LogUploadBtn");
+const LogUploadDialog = document.getElementById("LogUploadDialog");
+
+LogUploadBtn.addEventListener("click", () => {
+  LogUploadDialog.style.display = "block";
+  console.log(1);
+});
+
+const closeBtns = document.getElementsByClassName("closeBtn");
+
+Array.from(closeBtns).forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const dialog = btn.closest(".dialog");
+    dialog.style.display = "none";
+  });
+});
+
+const LogConfirmBtn = document.getElementById("LogConfirmBtn");
+LogConfirmBtn.addEventListener("click", () => {
+  const FileInput = document.getElementById("FileInput");
+  const file = FileInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const logContent = event.target.result;
+      console.log(logContent);
+    };
+
+    reader.readAsText(file);
+  } else {
+    alert("파일을 선택하세요.");
+  }
+});
